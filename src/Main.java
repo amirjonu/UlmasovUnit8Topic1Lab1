2489eaa5781a52842298116eee367a6e284f831c
amirjon <amirjonu@nycstudents.net>
2023-03-06 16:13:28 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] seatingChart= {{"Abby", "Don", "George", "Kim"}, {"Brian", "Elenor", "Harry", "Lenny"}, {"Cathy", "Fred", "Jill", "Matt"}};
+        seatingChart[1][2]= "Paul";
+        String temp= seatingChart[0][0];
+        seatingChart[0][0]= seatingChart[2][3];
+        seatingChart[2][3]= temp; 
+        String[] temp1= seatingChart[0];
+        seatingChart[0]= seatingChart[1];
+        seatingChart[1]= temp1;
+        for (String[] r: seatingChart){
+            System.out.println(Arrays.toString(r));
+        }
+        System.out.println(seatingChart[0][2] + seatingChart[2][0]);
+
+        int[][] arr1= new int[2][3];
+        int[][] arr2= new int[3][2];
+
+        arr1[0][0]= 1;
+        arr1[0][1]= 2;
+        arr1[0][2]= 3;
+        arr1[1][0]= 4;
+        arr1[1][1]= 5;
+        arr1[1][2]= 6;
+        arr2[0][0]= 1;
+        arr2[0][1]= 4;
+        arr2[1][0]= 2;
+        arr2[1][1]= 5;
+        arr2[2][0]= 3;
+        arr2[2][1]= 6;
+
+        for (int[] r: arr1){
+            System.out.println(Arrays.toString(r));
+        }
+        for (int[] r: arr2){
+            System.out.println(Arrays.toString(r));
+        }
+        System.out.println(arr1[0][2] + arr2[2][0]);
+
+
     }
 }
